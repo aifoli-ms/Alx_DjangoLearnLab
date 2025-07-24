b4307ce01e6b5fe5ab01827d2834cd8355cb75cc
Aifoli-Codex <codexaifoli@gmail.com>
2025-07-24 10:51:31 +0000

    mod

---
+++ django_models/relationship_app/models.py
@@ -0,0 +1,36 @@
+# relationship_app/models.py
+from django.db import models
+
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book Model (ForeignKey Relationship)
+# A Book has one Author, but an Author can have many Books.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+# Library Model (ManyToMany Relationship)
+# A Library can have many Books, and a Book can be in many Libraries.
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+# Librarian Model (OneToOne Relationship)
+# A Library has exactly one Librarian, and a Librarian manages exactly one Library.
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
